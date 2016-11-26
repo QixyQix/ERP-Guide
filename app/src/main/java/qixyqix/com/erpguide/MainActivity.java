3cf68ca19c240d74a34ffd880b4ab18f86c401f8
QixyQix <QixyQix@outlook.com>
2016-11-26 11:05:41 +0800

    Main Screen
    Done Main Screen
    + Tabhost for erp locations & top up locations
    + bottom menu bar
    
    Bugs:
    + Splash screen only shows animation and the 2nd splash image less than
    a second then goes to main activity

--- app/src/main/java/qixyqix/com/erpguide/MainActivity.java
+++ app/src/main/java/qixyqix/com/erpguide/MainActivity.java
@@ -2,6 +2,7 @@ package qixyqix.com.erpguide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,25 +14,42 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    GoogleMap googleMap;
+    GoogleMap erpLocationMap;
+    GoogleMap topUpLocationMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost host = (TabHost)findViewById(R.id.tabHostMainActivity);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("ERP Locations");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("Tab Two");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("TopUp Locations");
+        host.addTab(spec);
+
         initializeMap();
     }
 
     //Reference for the initialize map code
     //https://www.youtube.com/watch?v=lchyOhPREh4
     private void initializeMap() {
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragmentMainActivity);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragmentMapERPLocation);
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
+        //Set up ERP Location Map
+        this.erpLocationMap = googleMap;
         //Zoom to marina bay area aka downtown area aka where most of the ERPs are at
         zoomToLocation(1.287268, 103.855933,12);
 
@@ -49,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void zoomToLocation(double lat, double lng, float zoom){
         LatLng latLng = new LatLng(lat,lng);
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,zoom);
-        googleMap.moveCamera(cameraUpdate);
+        erpLocationMap.moveCamera(cameraUpdate);
 
     }
 }
--- app/src/main/java/qixyqix/com/erpguide/Splash.java
+++ app/src/main/java/qixyqix/com/erpguide/Splash.java
@@ -1,39 +1,69 @@
 package qixyqix.com.erpguide;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-/**
- * Created by QiXiang on 19/11/2016.
- */
+import java.util.concurrent.TimeUnit;
 
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity implements Animation.AnimationListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Launch splash layout
-        setContentView(R.layout.splash);
-        Thread splashThread = new Thread() {
+        setContentView(R.layout.activity_splash);
+    }
 
-            public void run() {
-                try {
-                    //3 seconds
-                    sleep(3000);
-                }  catch(Exception e) {
-                    //Show error
-                    e.printStackTrace();
-                } finally
-                {
-                    //Start the main activity
-                    Intent intent = new Intent(Splash.this, MainActivity.class);
-                    startActivity(intent);
-                }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startSplash();
+    }
 
-            }
-        };
-        // To Start the thread
-        splashThread.start();
+    private void startSplash(){
+        ImageView splashImageView = (ImageView) findViewById(R.id.imageViewSplash);
+
+        splashImageView.setBackgroundResource(R.mipmap.splashpt1);
+
+        splashImageView.setVisibility(View.VISIBLE);
+
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(5));
+
+        splashImageView.setBackgroundResource(R.mipmap.splashpt2);
+
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(4));
+
+        Animation anim = AnimationUtils.loadAnimation(this,R.anim.zoom_in);
+
+        anim.setDuration(300);
+
+        anim.setAnimationListener(this);
+
+        splashImageView.startAnimation(anim);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        //Start the main activity
+        Intent intent = new Intent(Splash.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
-
