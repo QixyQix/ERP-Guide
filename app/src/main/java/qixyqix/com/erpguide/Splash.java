4e145b14c73bdaab828dda80dc51f0d1b1a698f3
QixyQix <QixyQix@outlook.com>
2016-11-28 09:22:35 +0800

    Ver 0.1
    + Proper working splash screen
    + Main UI

--- app/src/main/java/qixyqix/com/erpguide/Splash.java
+++ app/src/main/java/qixyqix/com/erpguide/Splash.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 
 import java.util.concurrent.TimeUnit;
 
-public class Splash extends AppCompatActivity implements Animation.AnimationListener {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,46 +24,52 @@ public class Splash extends AppCompatActivity implements Animation.AnimationList
     @Override
     protected void onStart() {
         super.onStart();
-        startSplash();
+
+        //Splash Screen thread
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(2500); //Sleep for 2.5seconds
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Get the imageView
+                            ImageView imageView = (ImageView) findViewById(R.id.imageViewSplash);
+                            //Set it to the second image
+                            imageView.setBackgroundResource(R.mipmap.splashpt2);
+                        }
+                    });
+                    sleep(500); //Sleep for half a second
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //Load animation
+                            Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+                            //Set duration to 3 seconds (but it will move to the next activity halfway)
+                            animation.setDuration(3000);
+                            //Get the imageView
+                            ImageView imageView = (ImageView) findViewById(R.id.imageViewSplash);
+                            //Start the animation
+                            imageView.startAnimation(animation);
+                        }
+                    });
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally{
+                    //Create new intent for main activity
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    //Start the activity
+                    startActivity(intent);
+                    //close this activity (So user cannot go back)
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
     }
 
-    private void startSplash(){
-        ImageView splashImageView = (ImageView) findViewById(R.id.imageViewSplash);
-
-        splashImageView.setBackgroundResource(R.mipmap.splashpt1);
-
-        splashImageView.setVisibility(View.VISIBLE);
-
-        SystemClock.sleep(TimeUnit.SECONDS.toMillis(5));
-
-        splashImageView.setBackgroundResource(R.mipmap.splashpt2);
-
-        SystemClock.sleep(TimeUnit.SECONDS.toMillis(4));
-
-        Animation anim = AnimationUtils.loadAnimation(this,R.anim.zoom_in);
-
-        anim.setDuration(300);
-
-        anim.setAnimationListener(this);
-
-        splashImageView.startAnimation(anim);
     }
-
-    @Override
-    public void onAnimationStart(Animation animation) {
-
-    }
-
-    @Override
-    public void onAnimationEnd(Animation animation) {
-        //Start the main activity
-        Intent intent = new Intent(Splash.this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    @Override
-    public void onAnimationRepeat(Animation animation) {
-
-    }
-}
